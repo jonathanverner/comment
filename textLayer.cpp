f8286b0121cdb50350a8d541e0ee2964c95fd4a9
Jonathan Livingston Verner <joni@galadriel.(none)>
2008-11-09 00:59:15 +0100

    Made getIndex faster (in preparation of possible large lines, when each character will be in its own box). Added (a commented out) code to allow selecting single characters (however we need to wait for a newer version of poppler). Fixed backward selections.

--- textLayer.cpp
+++ textLayer.cpp
@@ -20,8 +20,8 @@ using namespace Poppler;
 
 class line { 
 	private:
+		QString text;
 		QVector<TextBox*> boxes;
-		QVector<QString> textBoxes;
 		QVector<QRectF> rectBoxes;
 		int sz;
 		int getIndex( qreal pos );
@@ -47,11 +47,26 @@ line::line(): sz(0), minY(-1), maxY(-1) {
 };
 
 void line::add( TextBox *bx ) { 
-  boxes.append( bx );
-  textBoxes.append( bx->text() );
-  rectBoxes.append( bx->boundingBox() );
+  QString txt=bx->text();
+  int tsz=txt.size();
   qreal lY = bx->boundingBox().y();
   qreal uY = bx->boundingBox().y()+bx->boundingBox().height();
+
+  boxes.append( bx );
+  text+=txt;
+
+  /* We have too old a version, cannot do character-level selections:
+   *
+      for(int i=0;i<tsz;i++) rectBoxes.append( bx->charBoundingBox(i) );
+      sz+=tsz;
+   * 
+   * For now we just do 
+   */
+      rectBoxes.append(bx->boundingBox());
+      sz++;
+   /*
+    * */
+
   if ( minY == -1 ) {
     minY = lY;
     maxY = uY;
@@ -59,7 +74,6 @@ void line::add( TextBox *bx ) {
     if ( lY < minY ) minY = lY;
     if ( uY > maxY ) maxY = uY;
   }
-  sz++;
 };
 
 int line::relativePosition( qreal y ) { 
@@ -68,25 +82,43 @@ int line::relativePosition( qreal y ) {
  return 0;
 }
 
-int line::getIndex( qreal pos ) {
+/*int line::getIndex( qreal pos ) {
   Q_ASSERT( sz > 0 );
   int ret=0;
   qreal cur_pos = rectBoxes[ret].x();
   while( cur_pos < pos && ret < sz) cur_pos = rectBoxes[ret++].x();
   if ( ret == sz ) return sz-1;
   return ret;
-}
+}*/
+
+int line::getIndex( qreal pos ) { 
+  Q_ASSERT( sz > 0 );
+  int min = 0, max = sz-1, pivot=min+(max-min)/2, dist;
+  qreal minPivotVal,maxPivotVal;
+  while( min < max ) { 
+    dist=(max-min);
+    minPivotVal = rectBoxes[pivot].x();
+    maxPivotVal = minPivotVal+rectBoxes[pivot].width();
+    if ( pos < minPivotVal ) max = pivot;
+    else if ( maxPivotVal < pos ) min = pivot;
+    else return pivot;
+    pivot = min+(max-min)/2;
+    if ( (max-min) >=dist ) return max;
+  }
+  return pivot;
+};
 
 QList<QRectF> line::all() {
   return QList<QRectF>::fromVector( rectBoxes );
 }
 
 QList<QRectF> line::toEnd( qreal from ) { 
-  return intervalBoxes( getIndex(from), sz );
+  return intervalBoxes( getIndex(from), sz-1 );
 };
 
 QList<QRectF> line::fromStart( qreal to ) { 
-  return interval( 0, getIndex(to) );
+  qDebug() << " From Start to " << getIndex(to) <<"("<<to<<")";
+  return intervalBoxes( 0, getIndex(to) );
 };
 QList<QRectF> line::interval( qreal from, qreal to ) { 
   return intervalBoxes( getIndex(from), getIndex(to) );
@@ -95,9 +127,12 @@ QList<QRectF> line::interval( qreal from, qreal to ) {
 
 QList<QRectF> line::intervalBoxes( int s, int e ) { 
  QList<QRectF> ret;
- for(int i=s;i<e;i++) {
+ QString txt="";
+ for(int i=s;i<=e;i++) {
    ret+=rectBoxes[i];
+   txt+=boxes[i]->text()+" ";
  }
+ qDebug() << "Selected text ("<<s<<"-"<<e<<"):"<<txt;
  return ret;
 };
 
@@ -117,11 +152,10 @@ textLayer::textLayer( Page *pg ) {
 }
 
 int textLayer::findLine( qreal y ) { 
-  int min = 0, max = lines.size(), pivot=min+(max-min)/2;
-  int relPos;
+  int min = 0, max = lines.size()-1, pivot=min+(max-min)/2;
   Q_ASSERT(max > min); 
   if ( lines[min]->relativePosition( y ) == -1 ) return 0;
-  else if ( lines[max-1]->relativePosition( y ) == 1 ) return max-1;
+  else if ( lines[max]->relativePosition( y ) == 1 ) return max-1;
   while( min < max ) { 
     switch(lines[pivot]->relativePosition( y )) { 
 	    case -1:
@@ -135,21 +169,27 @@ int textLayer::findLine( qreal y ) {
     };
     pivot = min+(max-min)/2;
   }
+  return pivot;
 };
 
 QList<QRectF> textLayer::select( QPointF from, QPointF to ) { 
  int startLine = findLine( from.y() ), endLine = findLine( to.y() );
+ qreal slineX = from.x(), elineX = to.x();
  if ( startLine > endLine ) {
-   startLine +=endLine;
-   endLine = startLine-endLine;
-   startLine -=endLine;
+   startLine +=endLine; slineX += elineX;
+   endLine = startLine-endLine; elineX = slineX-elineX;
+   startLine -=endLine; slineX -=elineX;
  }
  qDebug() << "testLayer::select " << startLine << " - " << endLine;
  QList<QRectF> ret;
- if ( startLine == endLine ) return lines[startLine]->interval( from.x(), to.x() );
- ret = lines[startLine]->toEnd( from.x() );
+  // The code needs to be checked for correctnes when we are in fact selecting backwards
+ if ( startLine == endLine ) {
+   if ( slineX < elineX ) return lines[startLine]->interval( slineX, elineX );
+   else return lines[startLine]->interval( elineX, slineX );
+ }
+ ret = lines[startLine]->toEnd( slineX );
  for(int i=startLine+1;i<endLine;i++ ) ret += lines[i]->all();
- ret += lines[endLine]->fromStart( to.x() );
+ ret += lines[endLine]->fromStart( elineX );
  return ret;
 };
 
