2b2530d0944a4d66c0dd96374d2e110c3415c42d
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2010-05-02 20:38:42 +0200

    Updated license information in cpp files.

--- textTool.cpp
+++ textTool.cpp
@@ -1,15 +1,28 @@
-/***************************************************************
- * textTool.cpp
- * @Author:      Jonathan Verner (jonathan.verner@matfyz.cz)
- * @License:     GPL v2.0 or later
- * @Created:     2008-10-17.
- * @Last Change: 2008-10-17.
- * @Revision:    0.0
- * Description:
- * Usage:
- * TODO:
- *CHANGES:
- ***************************************************************/
+/**  This file is part of project comment
+ *
+ *  File: textTool.cpp
+ *  Created: 2008-10-17
+ *  Author: Jonathan Verner <jonathan.verner@matfyz.cz>
+ *  License: GPL v2 or later
+ *
+ *  Copyright (C) 2010 Jonathan Verner <jonathan.verner@matfyz.cz>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ */
+
 
 
 
